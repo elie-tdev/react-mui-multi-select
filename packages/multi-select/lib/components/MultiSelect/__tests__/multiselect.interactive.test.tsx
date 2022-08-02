816f066945babc86211530922e4cb51d4cc6e6dd
web <joujiyoshino.work@gmail.com>
2022-08-02 05:05:39 +0200

    update testing to 100 percent

--- packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.interactive.test.tsx
+++ packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.interactive.test.tsx
@@ -4,29 +4,25 @@ import { MultiSelect } from '..'
 import { top100Films, Film } from './multiselect.layout.test'
 
 describe('Dropdown', () => {
-  it('value should be updated on change', () => {
-    function MultiSelectWrapper() {
-      const [value, setValue] = React.useState<Film[]>([])
-      const onChange = (e: Film[]) => {
-        setValue(e)
-      }
-      return (
-        <MultiSelect
-          value={value}
-          options={top100Films}
-          getOptionLabel={(u: Film) => u.title}
-          label="Films"
-          placeholder="Braveheart"
-          onChange={onChange}
-          getOptionKey={(u: Film) => u.title}
-        />
-      )
-    }
+  it('value should be updated on change', async () => {
+    const onChange = jest.fn()
+    const { getByRole } = render(
+      <MultiSelect
+        value={[]}
+        options={top100Films}
+        getOptionLabel={(u: Film) => u.title}
+        label="Films"
+        placeholder="Braveheart"
+        onChange={onChange}
+        getOptionKey={(u: Film) => u.title}
+      />,
+    )
 
-    const { getByRole } = render(<MultiSelectWrapper />)
     const element = getByRole('combobox', { name: 'Films' })
     fireEvent.focus(element)
-    fireEvent.change(element, { target: { value: 'The Godfather' } })
-    expect(element.getAttribute('value')).toBe('The Godfather')
+    fireEvent.change(element, { target: { value: top100Films[1].title } })
+    fireEvent.keyDown(element, { key: 'ArrowDown' })
+    fireEvent.keyDown(element, { key: 'Enter' })
+    expect(onChange).toBeCalledWith([top100Films[1]])
   })
 })
