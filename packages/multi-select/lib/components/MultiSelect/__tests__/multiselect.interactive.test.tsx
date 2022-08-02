c25ffab11258c2d1af1b1841ef3736672472647c
web <joujiyoshino.work@gmail.com>
2022-08-02 04:41:17 +0200

    updated test

--- packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.interactive.test.tsx
+++ packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.interactive.test.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { fireEvent, render } from '@testing-library/react'
-
 import { MultiSelect } from '..'
-import { top100Films, Film } from '../mockdata'
+import { top100Films, Film } from './multiselect.layout.test'
 
 describe('Dropdown', () => {
   it('value should be updated on change', () => {
-    const getOptionLabel = (u: Film) => u.title
-    const { getByRole } = render(
-      <MultiSelect
-        options={top100Films}
-        getOptionLabel={getOptionLabel}
-        label="Films"
-        placeholder="Braveheart"
-      />,
-    )
+    function MultiSelectWrapper() {
+      const [value, setValue] = React.useState<Film[]>([])
+      const onChange = (e: Film[]) => {
+        setValue(e)
+      }
+      return (
+        <MultiSelect
+          value={value}
+          options={top100Films}
+          getOptionLabel={(u: Film) => u.title}
+          label="Films"
+          placeholder="Braveheart"
+          onChange={onChange}
+          getOptionKey={(u: Film) => u.title}
+        />
+      )
+    }
 
+    const { getByRole } = render(<MultiSelectWrapper />)
     const element = getByRole('combobox', { name: 'Films' })
     fireEvent.focus(element)
     fireEvent.change(element, { target: { value: 'The Godfather' } })
--- packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.layout.test.tsx
+++ packages/multi-select/lib/components/MultiSelect/__tests__/multiselect.layout.test.tsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { MultiSelect } from '..'
-import { top100Films, Film } from '../mockdata'
+
+export type Film = {
+  title: string
+  year: number
+}
+
+export const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+]
 
 describe('MultiSelect', () => {
   it('should render successfully: ', () => {
-    const getOptionLabel = (u: Film) => u.title
-    const { container } = render(
-      <MultiSelect
-        options={top100Films}
-        getOptionLabel={getOptionLabel}
-        label="Films"
-        placeholder="Braveheart"
-      />,
-    )
+    function MultiSelectWrapper() {
+      const [value, setValue] = React.useState<Film[]>([])
+      const onChange = (e: Film[]) => {
+        setValue(e)
+      }
+      return (
+        <MultiSelect
+          value={value}
+          options={top100Films}
+          getOptionLabel={(u: Film) => u.title}
+          label="Films"
+          placeholder="Braveheart"
+          onChange={onChange}
+          getOptionKey={(u: Film) => u.title}
+        />
+      )
+    }
+
+    const { container } = render(<MultiSelectWrapper />)
     expect(container).toMatchSnapshot()
     expect(container).toBeTruthy()
   })
